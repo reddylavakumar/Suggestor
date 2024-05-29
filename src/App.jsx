f25d18c7f75a5c5e2ce33f44039b23a8fd8a47b7
reddylavakumar <reddylavakumar2001@gmailcom>
2024-05-29 22:48:11 +0530

    added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Homepage from './Components/Homepage/Homepage'
 import Header from './Components/Header and Footer/Header'
+import Footer from './Components/Header and Footer/Footer'
 import Landingpage from './Components/Landingpage/Landingpage'
 const App = () => {
   return (
     <>
     <Header/>
     <Homepage/>
+    <Footer />
     </>
   )
 }
